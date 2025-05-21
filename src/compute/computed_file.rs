6b8d361447f11c02f905e2ae3f1a23e5ecf4df97
Natchica <nathan.gaud@gmail.com>
2025-05-21 18:06:54 +0200

    test: Add unit tests

--- src/compute/computed_file.rs
+++ src/compute/computed_file.rs
@@ -4,7 +4,7 @@ use log::{error, info};
 use serde::{Deserialize, Serialize};
 use std::{fs, path::Path};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct ComputedFile {
     #[serde(rename = "deterministicOutputPath")]
     pub deterministic_output_path: Option<String>,
@@ -47,15 +47,16 @@ pub fn read_computed_file(
     }
 
     let computed_file_path = Path::new(computed_file_dir).join("computed.json");
-    let json_string = fs::read_to_string(&computed_file_path).unwrap();
-
-    if json_string.is_empty() {
-        error!(
-            "Failed to read compute file (invalid path) [chain_task_id:{}, computed_file_dir:{}]",
-            chain_task_id, computed_file_dir
-        );
-        return Err(ReplicateStatusCause::PostComputeComputedFileNotFound);
-    }
+    let json_string = match fs::read_to_string(&computed_file_path) {
+        Ok(content) => content,
+        Err(e) => {
+            error!(
+                "Failed to read compute file [chain_task_id:{}, computed_file_dir:{}, error:{}]",
+                chain_task_id, computed_file_dir, e
+            );
+            return Err(ReplicateStatusCause::PostComputeComputedFileNotFound);
+        }
+    };
 
     match serde_json::from_str::<ComputedFile>(&json_string) {
         Ok(mut computed_file) => {
@@ -94,4 +95,177 @@ pub fn build_result_digest_in_computed_file(
 
     computed_file.result_digest = Some(result_digest.to_string());
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Write;
+    use tempfile::tempdir;
+
+    // region read_computed_file
+    #[test]
+    fn read_computed_file_returns_computed_file_when_valid_input() {
+        let dir = tempdir().unwrap();
+        let dir_path = dir.path().to_str().unwrap();
+        let file_path = dir.path().join("computed.json");
+
+        let test_json =
+            r#"{"deterministicOutputPath":"/iexec_out/result.txt","callbackData":"0xabc"}"#;
+        let mut file = fs::File::create(&file_path).unwrap();
+        file.write_all(test_json.as_bytes()).unwrap();
+
+        let result = read_computed_file("0x123", dir_path);
+        assert!(result.is_ok());
+
+        let computed_file = result.unwrap();
+        assert_eq!(computed_file.task_id, Some("0x123".to_string()));
+        assert_eq!(
+            computed_file.deterministic_output_path,
+            Some("/iexec_out/result.txt".to_string())
+        );
+        assert_eq!(computed_file.callback_data, Some("0xabc".to_string()));
+    }
+
+    #[test]
+    fn read_computed_file_returns_error_when_chain_task_id_is_empty() {
+        let result = read_computed_file("", "/tmp");
+
+        assert!(result.is_err());
+        assert_eq!(
+            result.unwrap_err(),
+            ReplicateStatusCause::PostComputeComputedFileNotFound
+        );
+    }
+
+    #[test]
+    fn read_computed_file_returns_error_when_computed_file_dir_is_empty() {
+        let result = read_computed_file("0x123", "");
+
+        assert!(result.is_err());
+        assert_eq!(
+            result.unwrap_err(),
+            ReplicateStatusCause::PostComputeComputedFileNotFound
+        );
+    }
+
+    #[test]
+    fn read_computed_file_returns_error_when_computed_json_is_missing() {
+        let dir = tempdir().unwrap();
+        let dir_path = dir.path().to_str().unwrap();
+
+        let result = read_computed_file("0x123", dir_path);
+
+        assert!(result.is_err());
+        assert_eq!(
+            result.unwrap_err(),
+            ReplicateStatusCause::PostComputeComputedFileNotFound
+        );
+    }
+
+    #[test]
+    fn read_computed_file_returns_error_when_computed_json_is_empty() {
+        let dir = tempdir().unwrap();
+        let file_path = dir.path().join("computed.json");
+
+        let test_json = "";
+        let mut file = fs::File::create(&file_path).unwrap();
+        file.write_all(test_json.as_bytes()).unwrap();
+
+        let result = read_computed_file("0x123", dir.path().to_str().unwrap());
+
+        assert!(result.is_err());
+        assert_eq!(
+            result.unwrap_err(),
+            ReplicateStatusCause::PostComputeComputedFileNotFound
+        );
+    }
+
+    #[test]
+    fn read_computed_file_returns_error_when_computed_json_is_invalid() {
+        let dir = tempdir().unwrap();
+        let file_path = dir.path().join("computed.json");
+
+        let test_json = r#"{"invalidJson":}"#;
+        let mut file = fs::File::create(&file_path).unwrap();
+        file.write_all(test_json.as_bytes()).unwrap();
+
+        let result = read_computed_file("0x123", dir.path().to_str().unwrap());
+
+        assert!(result.is_err());
+        assert_eq!(
+            result.unwrap_err(),
+            ReplicateStatusCause::PostComputeComputedFileNotFound
+        );
+    }
+    // endregion
+
+    // region build_result_digest_in_computed_file
+    #[test]
+    fn build_result_digest_in_computed_file_computes_web3_digest_when_is_callback_mode_is_true() {
+        let mut computed_file = ComputedFile {
+            task_id: Some("0x123".to_string()),
+            callback_data: Some(
+                "0x0000000000000000000000000000000000000000000000000000000000000001".to_string(),
+            ),
+            deterministic_output_path: None,
+            result_digest: None,
+            enclave_signature: None,
+            error_message: None,
+        };
+
+        let result = build_result_digest_in_computed_file(&mut computed_file, true);
+
+        assert!(result.is_ok());
+        assert_eq!(
+            computed_file.result_digest,
+            Some("0xcb371be217faa47dab94e0d0ff0840c6cbf41645f0dc1a6ae3f34447155a76f3".to_string())
+        );
+    }
+
+    #[test]
+    fn build_result_digest_in_computed_file_computes_web2_digest_when_is_callback_mode_is_false() {
+        let dir = tempdir().unwrap();
+        let output_dir = dir.path().join("output");
+        fs::create_dir(&output_dir).unwrap();
+
+        let test_file_path = output_dir.join("test.txt");
+        let mut file = fs::File::create(&test_file_path).unwrap();
+        file.write_all(b"test content").unwrap();
+
+        let mut computed_file = ComputedFile {
+            task_id: Some("0x123".to_string()),
+            callback_data: None,
+            deterministic_output_path: Some(output_dir.to_str().unwrap().to_string()),
+            result_digest: None,
+            enclave_signature: None,
+            error_message: None,
+        };
+
+        let result = build_result_digest_in_computed_file(&mut computed_file, false);
+
+        assert!(result.is_ok());
+        assert!(computed_file.result_digest.is_some());
+    }
+
+    #[test]
+    fn build_result_digest_in_computed_file_returns_error_when_result_digest_is_empty() {
+        let mut computed_file = ComputedFile {
+            task_id: Some("0x123".to_string()),
+            callback_data: None,
+            deterministic_output_path: Some("/non_existent_path".to_string()),
+            result_digest: None,
+            enclave_signature: None,
+            error_message: None,
+        };
+
+        let result = build_result_digest_in_computed_file(&mut computed_file, false);
+
+        assert!(result.is_err());
+        assert_eq!(
+            result.unwrap_err(),
+            ReplicateStatusCause::PostComputeResultDigestComputationFailed
+        );
+    }
+    // endregion
 }
--- src/compute/utils/result_utils.rs
+++ src/compute/utils/result_utils.rs
@@ -1,4 +1,4 @@
-use crate::compute::utils::hash_utils::concatenate_and_hash;
+use crate::compute::utils::hash_utils::{concatenate_and_hash, sha256};
 use crate::compute::{computed_file::ComputedFile, utils::hash_utils::keccak256};
 use log::error;
 use sha256::digest;
@@ -24,11 +24,11 @@ pub fn compute_web3_result_digest(computed_file: &ComputedFile) -> String {
         }
     };
 
-    keccak256(callback_data.clone())
+    keccak256(callback_data)
 }
 
 pub fn compute_web2_result_digest(computed_file: &ComputedFile) -> String {
-    let deterministic_output_path = match &computed_file.deterministic_output_path {
+    let host_deterministic_output_path = match &computed_file.deterministic_output_path {
         Some(path) => {
             if path.is_empty() {
                 error!(
@@ -37,16 +37,13 @@ pub fn compute_web2_result_digest(computed_file: &ComputedFile) -> String {
                 );
                 return "".to_string();
             } else {
-                path
+                Path::new(path)
             }
         }
         _ => {
             return "".to_string();
         }
     };
-
-    let host_deterministic_output_path =
-        Path::new(deterministic_output_path.trim_start_matches('/'));
 
     if !host_deterministic_output_path.exists() {
         error!(
@@ -80,7 +77,7 @@ pub fn sha256_file(file_path: &Path) -> String {
             return "".to_string();
         }
     };
-    format!("0x{}", digest(&data))
+    sha256(data)
 }
 
 pub fn get_file_tree_sha256(file_tree_path: &Path) -> String {
@@ -117,4 +114,242 @@ pub fn get_file_tree_sha256(file_tree_path: &Path) -> String {
     let hashes: Vec<&str> = hashes_vec.iter().map(|s| s.as_str()).collect();
     let hashes = hashes.as_slice();
     concatenate_and_hash(hashes)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Write;
+    use tempfile::tempdir;
+
+    #[test]
+    fn compute_web3_result_digest_returns_digest_when_valid_input() {
+        let computed_file = ComputedFile {
+            task_id: Some("0x123".to_string()),
+            callback_data: Some(
+                "0x0000000000000000000000000000000000000000000000000000000000000001".to_string(),
+            ),
+            deterministic_output_path: None,
+            result_digest: None,
+            enclave_signature: None,
+            error_message: None,
+        };
+
+        let result = compute_web3_result_digest(&computed_file);
+
+        assert_eq!(
+            result,
+            "0xcb371be217faa47dab94e0d0ff0840c6cbf41645f0dc1a6ae3f34447155a76f3"
+        );
+    }
+
+    #[test]
+    fn compute_web3_result_digest_returns_empty_string_when_task_id_is_none() {
+        let computed_file = ComputedFile {
+            task_id: None,
+            callback_data: Some("0xdata".to_string()),
+            deterministic_output_path: None,
+            result_digest: None,
+            enclave_signature: None,
+            error_message: None,
+        };
+
+        let result = compute_web3_result_digest(&computed_file);
+
+        assert_eq!(result, "");
+    }
+
+    #[test]
+    fn compute_web3_result_digest_returns_empty_string_when_callback_data_is_none() {
+        let computed_file = ComputedFile {
+            task_id: Some("0x123".to_string()),
+            callback_data: None,
+            deterministic_output_path: None,
+            result_digest: None,
+            enclave_signature: None,
+            error_message: None,
+        };
+
+        let result = compute_web3_result_digest(&computed_file);
+
+        assert_eq!(result, "");
+    }
+
+    #[test]
+    fn compute_web3_result_digest_returns_empty_string_when_callback_data_is_empty() {
+        let computed_file = ComputedFile {
+            task_id: Some("0x123".to_string()),
+            callback_data: Some("".to_string()),
+            deterministic_output_path: None,
+            result_digest: None,
+            enclave_signature: None,
+            error_message: None,
+        };
+
+        let result = compute_web3_result_digest(&computed_file);
+
+        assert_eq!(result, "");
+    }
+
+    #[test]
+    fn compute_web2_result_digest_returns_digest_when_valid_input() {
+        let dir = tempdir().unwrap();
+        let output_dir = dir.path().join("output");
+        fs::create_dir(&output_dir).unwrap();
+
+        let test_file_path = output_dir.join("test.txt");
+        let mut file = fs::File::create(&test_file_path).unwrap();
+        file.write_all(b"test content").unwrap();
+
+        let computed_file = ComputedFile {
+            task_id: Some("0x123".to_string()),
+            callback_data: None,
+            deterministic_output_path: Some(output_dir.to_str().unwrap().to_string()),
+            result_digest: None,
+            enclave_signature: None,
+            error_message: None,
+        };
+
+        let result = compute_web2_result_digest(&computed_file);
+
+        assert!(!result.is_empty());
+        assert!(result.starts_with("0x"));
+    }
+
+    #[test]
+    fn compute_web2_result_digest_returns_empty_string_when_deterministic_output_path_is_none() {
+        let computed_file = ComputedFile {
+            task_id: Some("0x123".to_string()),
+            callback_data: None,
+            deterministic_output_path: None,
+            result_digest: None,
+            enclave_signature: None,
+            error_message: None,
+        };
+
+        let result = compute_web2_result_digest(&computed_file);
+
+        assert_eq!(result, "");
+    }
+
+    #[test]
+    fn compute_web2_result_digest_returns_empty_string_when_deterministic_output_path_is_empty() {
+        let computed_file = ComputedFile {
+            task_id: Some("0x123".to_string()),
+            callback_data: None,
+            deterministic_output_path: Some("".to_string()),
+            result_digest: None,
+            enclave_signature: None,
+            error_message: None,
+        };
+
+        let result = compute_web2_result_digest(&computed_file);
+
+        assert_eq!(result, "");
+    }
+
+    #[test]
+    fn compute_web2_result_digest_returns_empty_string_when_host_deterministic_output_path_does_not_exist()
+     {
+        let computed_file = ComputedFile {
+            task_id: Some("0x123".to_string()),
+            callback_data: None,
+            deterministic_output_path: Some("/non_existent_path".to_string()),
+            result_digest: None,
+            enclave_signature: None,
+            error_message: None,
+        };
+
+        let result = compute_web2_result_digest(&computed_file);
+
+        assert_eq!(result, "");
+    }
+
+    #[test]
+    fn sha256_file_returns_digest_when_valid_input() {
+        let dir = tempdir().unwrap();
+        let file_path = dir.path().join("test.txt");
+
+        let mut file = fs::File::create(&file_path).unwrap();
+        file.write_all(b"test content").unwrap();
+
+        let result = sha256_file(&file_path);
+
+        assert!(!result.is_empty());
+        assert!(result.starts_with("0x"));
+    }
+
+    #[test]
+    fn sha256_file_returns_empty_string_when_file_is_empty() {
+        let dir = tempdir().unwrap();
+        let file_path = dir.path().join("empty.txt");
+
+        let mut file = fs::File::create(&file_path).unwrap();
+        file.write_all(b"").unwrap();
+
+        let result = sha256_file(&file_path);
+        assert!(result.is_empty());
+    }
+
+    #[test]
+    fn sha256_file_returns_empty_string_when_file_does_not_exist() {
+        let dir = tempdir().unwrap();
+        let file_path = dir.path().join("nonexistent.txt");
+
+        let result = sha256_file(&file_path);
+
+        assert_eq!(result, "");
+    }
+
+    #[test]
+    fn get_file_tree_sha256_returns_digest_when_input_is_file() {
+        let dir = tempdir().unwrap();
+        let file_path = dir.path().join("test.txt");
+
+        let mut file = fs::File::create(&file_path).unwrap();
+        file.write_all(b"test content").unwrap();
+
+        let result = get_file_tree_sha256(&file_path);
+
+        assert!(!result.is_empty());
+        assert!(result.starts_with("0x"));
+    }
+
+    #[test]
+    fn get_file_tree_sha256_returns_digest_when_input_is_directory() {
+        let dir = tempdir().unwrap();
+
+        // Create some files in the directory
+        let file_path1 = dir.path().join("file1.txt");
+        let mut file1 = fs::File::create(&file_path1).unwrap();
+        file1.write_all(b"content 1").unwrap();
+
+        let file_path2 = dir.path().join("file2.txt");
+        let mut file2 = fs::File::create(&file_path2).unwrap();
+        file2.write_all(b"content 2").unwrap();
+
+        let result = get_file_tree_sha256(dir.path());
+
+        assert!(!result.is_empty());
+        assert!(result.starts_with("0x"));
+    }
+
+    #[test]
+    fn get_file_tree_sha256_returns_empty_string_when_path_does_not_exist() {
+        let dir = tempdir().unwrap();
+        let nonexistent_path = dir.path().join("nonexistent");
+
+        let result = get_file_tree_sha256(&nonexistent_path);
+
+        assert_eq!(result, "");
+    }
+
+    #[test]
+    fn get_file_tree_sha256_returns_empty_string_when_directory_is_empty() {
+        let dir = tempdir().unwrap();
+
+        let result = get_file_tree_sha256(dir.path());
+
+        assert_eq!(result, "");
+    }
 }
