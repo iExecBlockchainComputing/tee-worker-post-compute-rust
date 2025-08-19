36f02b49a0e6a6494934a14ba0ab3e859525f1b8
Natchica <nathan.gaud@gmail.com>
2025-08-19 19:13:02 +0200

    chore: remove pre-compute and post-compute modules

--- post-compute/src/compute.rs
+++
@@ -1,8 +0,0 @@
-pub mod app_runner;
-pub mod computed_file;
-pub mod dropbox;
-pub mod encryption;
-pub mod errors;
-pub mod signer;
-pub mod utils;
-pub mod web2_result;
--- pre-compute/src/compute.rs
+++
@@ -1,6 +0,0 @@
-pub mod app_runner;
-pub mod errors;
-mod pre_compute_app;
-mod pre_compute_args;
-pub mod signer;
-pub mod utils;
