13089f6309a23e2f90112de7b6a50510bca5ff0a
Natchica <nathan.gaud@gmail.com>
2025-08-19 19:21:54 +0200

    chore: update workspace config to make compile work and be able to run the tests

--- compute/pre/src/bin/main.rs
+++ compute/pre/src/bin/main.rs
@@ -1,12 +1,10 @@
+use tee_worker_pre_compute::compute::app_runner::start;
 use env_logger::{Builder, Env, Target};
 use std::process;
-
-mod api;
-mod compute;
 
 fn main() {
     Builder::from_env(Env::default().default_filter_or("info"))
         .target(Target::Stdout)
         .init();
-    process::exit(compute::app_runner::start() as i32);
+    process::exit(start() as i32);
 }
